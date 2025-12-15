89ebf929b6aeb91cc5d97a34130589f2ec488fa0
Diraisiel <133325314+JotaFab@users.noreply.github.com>
2025-12-15 20:27:06 +0000

    day 1 2025 AOC

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -4,35 +4,53 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-
 	file, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		panic(err)
 	}
 	defer file.Close()
-	n := 50
 
-	// Read the file line by line
+	pos := 50
+	count := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// Split each line into two numbers
 		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
 
-		fmt.Println(line)
-		for l := range line {
-			fmt.Println(l)
+		dir := line[0]
+		dist, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
 
+		switch dir {
+		case 'L':
+			pos = (pos - dist) % 100
+		case 'R':
+			pos = (pos + dist) % 100
+		}
+
+		// Go can produce negative modulo results
+		if pos < 0 {
+			pos += 100
+		}
+
+		if pos == 0 {
+			count++
 		}
 	}
 
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(count)
 }
-
-
-func solver()
-
-
